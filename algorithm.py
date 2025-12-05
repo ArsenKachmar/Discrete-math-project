5c3a67429c884da078b9d209f01fa7ae34db9554
ArsenKachmar <a.kachmar23072008@gmail.com>
2025-12-05 14:18:21 +0200

    commit

--- algorithm.py
+++ algorithm.py
@@ -1,11 +1,10 @@
 def algorithm(matrix):
     vertices_number = len(matrix)
-    previous_matrix = [[v for v in range(vertices_number)] for u in range(vertices_number)]
-
     for k in range(vertices_number):
         for i in range(vertices_number):
             for j in range(vertices_number):
                 d = matrix[i][k] + matrix[k][j]
                 if matrix[i][j] > d:
                     matrix[i][j] = d
-                    previous_matrix[i][j] = k
+                if matrix[i][j] < 0:
+                    raise ValueError("The path cannot be smaller than 0")
