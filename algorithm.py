66357eb93ff39986d7209faa327042777c4bbf3f
ArsenKachmar <a.kachmar23072008@gmail.com>
2025-12-12 16:50:20 +0200

    commit

--- algorithm.py
+++ algorithm.py
@@ -3,8 +3,6 @@ def algorithm(matrix):
     for k in range(vertices_number):
         for i in range(vertices_number):
             for j in range(vertices_number):
-                d = matrix[i][k] + matrix[k][j]
-                if matrix[i][j] > d:
-                    matrix[i][j] = d
+                matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j])
                 if matrix[i][j] < 0:
                     raise ValueError("The path cannot be smaller than 0")
